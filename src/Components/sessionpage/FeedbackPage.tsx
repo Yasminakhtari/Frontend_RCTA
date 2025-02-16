7c1b37d76d583a22b9531b95289b732ccab50d18
Abinashpatri07 <abinashpatri5@gmail.com>
2025-02-16 19:14:54 +0530

    test 6

--- src/Components/sessionpage/FeedbackPage.tsx
+++ src/Components/sessionpage/FeedbackPage.tsx
@@ -18,7 +18,7 @@ const FeedbackPage: React.FC = () => {
       console.log(data.data);
       setFeedback(Array.isArray(data.data) ? data.data : []);
     } catch (error) {
-      console.error('Failed to fetch locations:', error);
+      console.error('Failed to fetch Feedback:', error);
     } finally {
       setLoading(false);
     }
