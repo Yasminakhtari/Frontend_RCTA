7ce91a7cdde73d012f4125456f1c577c4fd55023
sourava1 <sourava2345@gmail.com>
2025-03-21 17:31:50 +0530

    .

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,2 @@
-//export const base_url = "https://backend-rcta.onrender.com/api";
-export const  base_url = "http://localhost:8082/api"    
+export const base_url = "https://backend-rcta.onrender.com/api";
+//export const  base_url = "http://localhost:8082/api"    
